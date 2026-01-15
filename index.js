c184e0392ef38c844e2b99a6a0a1d64a86ab42dd
samssj10 <samiran0361@gmail.com>
2026-01-15 21:01:11 +0530

    added basic arithmetic functions and operate logic

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return b == 0 ? "divide by 0 error" : a / b;
+}
+
+let num1;
+let num2;
+let operator;
+
+function operate(num1, num2, operator) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+  }
+  return result;
+}
